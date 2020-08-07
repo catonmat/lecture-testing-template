ac79a66d1a49f608bc5c8c555744a02be9171832
richard jarram <rjarram@me.com>
2020-08-06 21:27:38 -0400

    1:4 BEGIN create controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-puts 'Creating seed'
+puts 'creating seed.'
 
 5.times do |i|
     product = Product.create(
         name: Faker::Company.name,
         tagline: Faker::Company.catch_phrase
     )
+
     puts "Created product! Number: #{i+1}, name: #{product.name}"
 end
 
-User.create(email: 'richard@email.com', first_name: 'richard', last_name: 'jarram', password: '123456')
-
-puts 'Seed completed'
+User.create!(email: 'richard@email.com', first_name: 'richard', last_name: 'jarram', password: '123456')
