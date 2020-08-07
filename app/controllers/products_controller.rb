dcc706934ebfd375da6b48af6c8f36c7efec5a29
richard jarram <rjarram@me.com>
2020-08-07 15:56:19 -0400

    1:5 HTML done for products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,6 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @product = Product.all
+    @products = Product.all
   end
 end
