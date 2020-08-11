65ae72617f32525d6e9a3b2a79c1788d901c135f
richard jarram <rjarram@me.com>
2020-08-11 16:40:12 -0400

    3:7. test written with launchy screenshot

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test 'that the index page loads' do
     visit '/'
+    save_and_open_screenshot
     assert_selector 'h1', text: 'Awesome Products'
   end
 end
