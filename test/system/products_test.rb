3a82b9807196d14bccafb1abdeb05024709cd2bd
richard jarram <rjarram@me.com>
2020-08-12 14:37:04 -0400

    4:3. COMPLETED TDD suite

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -7,4 +7,20 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'Awesome Products'
     assert_selector '.card-product', count: Product.count
   end
+
+  test 'that the user can create a new product' do
+    # step 1: user logs in
+    login_as users(:id_1)
+    # step 2: user visit 'new product' page
+    visit '/products/new'
+    save_and_open_screenshot
+    # step 3: user fills and submits new product form
+    fill_in 'product_name', with: 'my great product'
+    fill_in 'product_tagline', with: 'some catchy phrase'
+    save_and_open_screenshot
+    click_on 'Create Product'
+    
+    assert_equal root_path, page.current_path
+    assert_text 'some catchy phrase'
+  end
 end
